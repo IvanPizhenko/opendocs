42373fedd0a7a34b5fb93a6ad2fa80f47ca5f527
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2022-09-12 07:37:07 +0300

    Update git_aliases.sh

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Initial version
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
@@ -12,3 +13,8 @@ git config --global alias.mkbr 'checkout -b'
 git config --global alias.track 'checkout --track'
 git config --global alias.amd 'commit --amend --no-edit --date=now'
 git config --global alias.fpush 'push -f'
+
+# Additions 2022-09-12
+git config --global alias.rpo 'remote prune origin'
+git config --global alias.rmbr 'branch --delete'
+git config --global alias.am 'commit --amend -m'
