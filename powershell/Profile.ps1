b0ed0fe42411466c2d93a15bb20be3f0349ac490
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2023-08-18 21:47:11 +0300

    Update Profile.ps1

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -1,17 +1,17 @@
 function gitwip
 {
-	git status
-	git add -u
-	git commit -m wip
-	git push
+  git status
+  git add -u
+  git commit -m wip
+  git push
   get-date
 }
 
 function gitfix
 {
-	git status
-	git add -u
-	git commit -m fix
-	git push
+  git status
+  git add -u
+  git commit -m fix
+  git push
   get-date
 }
