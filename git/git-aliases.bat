508d7e56201d85e4837586bfaf8182eb3ee333a8
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2025-07-09 20:13:02 +0300

    Update git-aliases.bat

--- git/git-aliases.bat
+++ git/git-aliases.bat
@@ -5,6 +5,7 @@ git config --global alias.cl clone
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.cm commit -m
 git config --global alias.st status
 git config --global alias.unstage "reset HEAD --"
 git config --global alias.last "log -1 HEAD"
