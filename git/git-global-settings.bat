37efa47ea825496667a11b0ece6b02357ddaf831
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2025-07-21 15:43:22 +0300

    Create git-global-settings.bat

---
+++ git/git-global-settings.bat
@@ -0,0 +1,5 @@
+@echo off
+
+git config --global credential.https://github.com.useHttpPath true
+git config --global credential.https://gitlab.com.useHttpPath true
+git config --global credential.https://bitbucket.org.useHttpPath true
