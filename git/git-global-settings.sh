4b9a77a702885d25ae7344b76dd737db0612707e
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2025-08-01 10:11:37 +0300

    Update git-global-settings.sh

--- git/git-global-settings.sh
+++ git/git-global-settings.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# Site settings
 git config --global credential.https://github.com.useHttpPath true
 git config --global credential.https://gitlab.com.useHttpPath true
 git config --global credential.https://bitbucket.org.useHttpPath true
+
+# Store credentials
+git config --global credential.helper store
+
+# END
