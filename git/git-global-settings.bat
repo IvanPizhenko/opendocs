911163a95ab5b406a38e0add56a783c945039d87
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2025-08-01 10:10:57 +0300

    Update git-global-settings.bat

--- git/git-global-settings.bat
+++ git/git-global-settings.bat
@@ -1,5 +1,9 @@
 @echo off
 
+:: Site setting
 git config --global credential.https://github.com.useHttpPath true
 git config --global credential.https://gitlab.com.useHttpPath true
 git config --global credential.https://bitbucket.org.useHttpPath true
+
+:: Store credentials
+git config --global credential.helper store
