07a0b75a5e2bd08e41b128f940655872d6431ef0
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2025-08-06 16:20:39 +0300

    wip

--- virt/vmware/mount-vmshare.sh
+++ virt/vmware/mount-vmshare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
+# More information:
+# https://gcore.com/learning/what-is-shared-folder-vmware-linux
+
 echo "Checking permissions..."
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root or using sudo." >&2
