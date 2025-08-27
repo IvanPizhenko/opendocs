acc1e56571c91978f6a1870c2cc25e0d18ee7e6f
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2025-08-27 11:46:17 +0300

    Add git alias for cherry-pick command

--- git/git-aliases.sh
+++ git/git-aliases.sh
@@ -21,6 +21,7 @@ git config --global alias.rpu 'remote prune upstream'
 git config --global alias.rmrb 'push origin --delete'
 git config --global alias.rmrt 'push origin --delete'
 git config --global alias.lsr 'ls-remote'
+git config --global alias.chp 'cherry-pick'
 # end
 
 # to list git aliases
