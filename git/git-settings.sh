2802020a4963a512a4023b0b782bc78a12a45cac
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2025-10-12 16:59:33 +0300

    Update git-settings.sh for credential management
    Comment out the credential store configuration and enable the credential manager with GPG.

--- git/git-settings.sh
+++ git/git-settings.sh
@@ -7,6 +7,10 @@ git config --global credential.https://gitlab.com.useHttpPath true
 git config --global credential.https://bitbucket.org.useHttpPath true
 
 # Store credentials
-git config --global credential.helper store
+# git config --global credential.helper store
+
+# Store credential with git-credentials-manager
+git config --global credential.helper manager
+git config --global credential.credentialStore gpg
 
 # END
