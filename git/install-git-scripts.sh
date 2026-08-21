f7e5a79596da7de23a6a3d6d352b3c7189207583
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2026-08-21 07:25:04 +0300

    Update git scripts

--- git/install-git-scripts.sh
+++ git/install-git-scripts.sh
@@ -11,7 +11,7 @@ tdir=/usr/local/bin
 echo "Creating $tdir directory"
 install -d $tdir
 
-for f in gitwip gitfix; do
+for f in gitwip gitwip2 gitfix gitfix2; do
     echo "Installing $f into $tdir"
     install $f $tdir
 done
